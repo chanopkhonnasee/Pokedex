307c6850fe11c8a839f6823b34804d256f8c67ae
chanop khonnasee <chanop.k@cdg.co.th>
2021-04-26 20:18:03 +0700

    first adapter

---
+++ app/src/main/java/com/finnomena/project/candidate/ui/ListAdapter.kt
@@ -0,0 +1,44 @@
+package com.finnomena.project.candidate.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.properties.Delegates
+
+import android.content.Context
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.finnomena.project.candidate.R
+import com.finnomena.project.candidate.repositiry.model.PokemonEntry
+
+class ListAdapter() : RecyclerView.Adapter<ListAdapter.ListResultViewHolder>() {
+
+    var listData: List<PokemonEntry> by Delegates.observable(listOf()) { _, _, _ -> notifyDataSetChanged() }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+    override fun onBindViewHolder(holder: ListResultViewHolder, position: Int) {
+        val item = listData[position]
+        item.pokemonSpecies?.name?.let { holder.tvPokemonName.text = it }
+        item.entryNumber?.let { holder.tvNumber.text = it.toString() }
+        holder.layoutPokemon.setOnClickListener {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListResultViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_holder_list, parent, false)
+        return ListResultViewHolder(view, this)
+    }
+
+    inner class ListResultViewHolder(itemView: View, private val adapter: ListAdapter) :
+        RecyclerView.ViewHolder(itemView) {
+        internal val tvPokemonName: TextView = itemView.findViewById(R.id.tvPokemonName)
+        internal val tvNumber: TextView = itemView.findViewById(R.id.tvNumber)
+        internal val layoutPokemon: ConstraintLayout = itemView.findViewById(R.id.layoutPokemon)
+    }
+
+}
--- app/src/main/java/com/finnomena/project/candidate/ui/MainActivity.kt
+++ app/src/main/java/com/finnomena/project/candidate/ui/MainActivity.kt
@@ -2,15 +2,37 @@ package com.finnomena.project.candidate.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.finnomena.project.candidate.R
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModel<MainViewModel>()
 
+    private var pokemonAdapter: ListAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewData()
+        initEvent()
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel.pokemonKanto.observe(this, Observer { kanto ->
+            kanto.pokemonEntries?.let { pokemonEntry -> pokemonAdapter?.listData = pokemonEntry }
+        })
+    }
+
+    private fun initEvent() {
+
+    }
+
+    private fun initViewData() {
+        pokemonAdapter = ListAdapter()
+        listRecycler.adapter = pokemonAdapter
 
         viewModel.getPokemonKanto()
     }
